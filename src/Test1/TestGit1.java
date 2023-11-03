585612260521d891bb061084341d4b337a4ffc7a
Tirkish M <tirkish1989@gmail.com>
2023-11-03 20:10:01 +0100

    TestGit1 Updated

---
+++ src/Test1/TestGit1.java
@@ -0,0 +1,13 @@
+package Test1;
+
+public class TestGit1 {
+    public static void main(String[] args) {
+
+        System.out.println(" Git!!");
+        System.out.println(" Test1");
+        System.out.println(" Test2");
+        System.out.println("Test3");
+        System.out.println("Test4");
+
+    }
+}
