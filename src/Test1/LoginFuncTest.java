7756a65c45731e3cefb0ccc05e0cd96429106864
Tirkish M <tirkish1989@gmail.com>
2023-11-07 21:21:34 +0100

    Local test file uploaded

---
+++ src/Test1/LoginFuncTest.java
@@ -0,0 +1,9 @@
+package Test1;
+
+public class LoginFuncTest {
+    public static void main(String[] args) {
+
+        System.out.println("navigate to login page");
+        System.out.println("enter username");
+    }
+}
